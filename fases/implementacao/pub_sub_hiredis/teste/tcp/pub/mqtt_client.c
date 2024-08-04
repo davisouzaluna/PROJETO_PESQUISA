f1144efcbe62ae64b6f1ccc6215b83746031e00c
Davi Luna <Sdavi738@gmail.com>
2024-08-03 21:23:00 -0300

    delete unnused methods

--- fases/implementacao/pub_sub_hiredis/teste/tcp/pub/mqtt_client.c
+++ fases/implementacao/pub_sub_hiredis/teste/tcp/pub/mqtt_client.c
@@ -13,7 +13,6 @@
 #include <string.h>
 #include <signal.h>
 #include <time.h>
-#include <hiredis/hiredis.h>
 
 #include "nng/mqtt/mqtt_client.h"
 #include "nng/nng.h"
@@ -34,67 +33,8 @@ void fatal(const char *msg, int rv) {
     fprintf(stderr, "%s: %s\n", msg, nng_strerror(rv));
 }
 
-typedef struct {
-    const char *value;
-    const char *redis_key;
-} RedisParams;
 
-void store_in_redis(const char *value, const char *redis_key) {
-    // Conectar ao servidor Redis na porta 6379 (padrão)
-    redisContext *context = redisConnect("127.0.0.1", 6379);
-    if (context == NULL || context->err) {
-        if (context) {
-            printf("Erro na conexão com o Redis: %s\n", context->errstr);
-            redisFree(context);
-        } else {
-            printf("Erro na alocação do contexto do Redis\n");
-        }
-        return;
-    }
 
-    printf("Conectado ao servidor Redis\n");
-
-    // Salvar o valor no Redis com a chave fornecida ou "valores" como padrão
-    const char *key = redis_key && *redis_key ? redis_key : "valores";
-    redisReply *reply = redisCommand(context, "RPUSH %s \"%s\"", key, value);
-    if (reply == NULL) {
-        printf("Erro ao salvar os dados no Redis\n");
-        redisFree(context);
-        return;
-    }
-    printf("Valor salvo com sucesso no Redis: %s ,na chave: %s\n", value, redis_key);
-    freeReplyObject(reply);
-
-    // Encerrar a conexão com o servidor Redis
-    redisFree(context);
-}
-
-void *store_in_redis_async(void *params) {
-    RedisParams *redis_params = (RedisParams *)params;
-    store_in_redis(redis_params->value, redis_params->redis_key);
-    free(params); // Libera a memória alocada para os parametros
-    return NULL;
-}
-
-void store_in_redis_async_call(const char *value, const char *redis_key) {
-    pthread_t thread;
-    RedisParams *params = malloc(sizeof(RedisParams));
-    if (params == NULL) {
-        perror("Erro ao alocar memória para os parâmetros da thread");
-        exit(EXIT_FAILURE);
-    }
-    params->value = value;
-    params->redis_key = redis_key;
-
-    if (pthread_create(&thread, NULL, store_in_redis_async, params) != 0) {
-        perror("Erro ao criar a thread");
-        free(params); // Libera a memória alocada em caso de falha na criação da thread
-        exit(EXIT_FAILURE);
-    }
-
-    // opcional: Se não precisar esperar a thread terminar, você pode desanexá-la, por enquanto preferi deixar a thread rodando
-    pthread_detach(thread);
-}
 
 long long tempo_atual_nanossegundos() {
     struct timespec tempo_atual;
