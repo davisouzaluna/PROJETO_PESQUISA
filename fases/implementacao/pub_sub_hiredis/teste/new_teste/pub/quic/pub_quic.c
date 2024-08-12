6c9476a08c93a26b611e9c024154e55df3c6765f
Davi Luna <Sdavi738@gmail.com>
2024-08-12 17:52:03 -0300

    Fix segmentation fault with Redis usage and improve thread handling
    - Fixed segmentation fault by properly allocating and duplicating strings for RedisParams.
    - Initialized redis_key with a default value if NULL.
    - Removed duplicate call to nng_mqtt_quic_client_open_conf.
    - Improved buffer initialization to avoid uninitialized pointer issues in sprintf.
    - Added error checking when creating the thread for asynchronous Redis storage.

--- fases/implementacao/pub_sub_hiredis/teste/new_teste/pub/quic/pub_quic.c
+++ fases/implementacao/pub_sub_hiredis/teste/new_teste/pub/quic/pub_quic.c
@@ -10,7 +10,6 @@
 #include "msquic.h"
 #include <pthread.h>
 #include <hiredis/hiredis.h>
-
 #include <time.h>
 
 #define MAX_STR_LEN 30
@@ -18,43 +17,39 @@
 #define CLOCK_REALTIME 0
 #endif
 
-
-static nng_socket * g_sock;
+static nng_socket *g_sock;
 
 #define CONN 1
 #define PUB 3
+
 struct timespec start_time, end_time;
 struct timespec start_time_pub, end_time_pub;
 double time_connection;
 
-
 conf_quic config_user = {
-	.tls = {
-		.enable = false,
-		.cafile = "",
-		.certfile = "",
-		.keyfile  = "",
-		.key_password = "",
-		.verify_peer = true,
-		.set_fail = true,
-	},
-	.multi_stream = false,
-	.qos_first  = false,
-	.qkeepalive = 30,
-	.qconnect_timeout = 60,//pode ser alterado
-	.qdiscon_timeout = 30,
-	.qidle_timeout = 30,
+    .tls = {
+        .enable = false,
+        .cafile = "",
+        .certfile = "",
+        .keyfile = "",
+        .key_password = "",
+        .verify_peer = true,
+        .set_fail = true,
+    },
+    .multi_stream = false,
+    .qos_first = false,
+    .qkeepalive = 30,
+    .qconnect_timeout = 60, // pode ser alterado
+    .qdiscon_timeout = 30,
+    .qidle_timeout = 30,
 };
-
 
 typedef struct {
     const char *value;
     const char *redis_key;
 } RedisParams;
 
-
 void store_in_redis(const char *value, const char *redis_key) {
-    // Conectar ao servidor Redis na porta 6379 (padrão)
     redisContext *context = redisConnect("127.0.0.1", 6379);
     if (context == NULL || context->err) {
         if (context) {
@@ -68,7 +63,6 @@ void store_in_redis(const char *value, const char *redis_key) {
 
     printf("Conectado ao servidor Redis\n");
 
-    // Salvar o valor no Redis com a chave fornecida ou "valores" como padrão
     const char *key = redis_key && *redis_key ? redis_key : "valores";
     redisReply *reply = redisCommand(context, "RPUSH %s \"%s\"", key, value);
     if (reply == NULL) {
@@ -76,18 +70,18 @@ void store_in_redis(const char *value, const char *redis_key) {
         redisFree(context);
         return;
     }
-    printf("Valor salvo com sucesso no Redis: %s ,na chave: %s\n", value, redis_key );
+    printf("Valor salvo com sucesso no Redis: %s ,na chave: %s\n", value, redis_key);
     freeReplyObject(reply);
 
-    // Encerrar a conexão com o servidor Redis
     redisFree(context);
 }
-
 
 void* store_in_redis_async(void *params) {
     RedisParams *redis_params = (RedisParams *)params;
     store_in_redis(redis_params->value, redis_params->redis_key);
-    free(params); // Libera a memória alocada para os parametros
+    free((char*)redis_params->value);
+    free((char*)redis_params->redis_key);
+    free(params); // Libera a memória alocada para os parâmetros
     return NULL;
 }
 
@@ -98,232 +92,167 @@ void store_in_redis_async_call(const char *value, const char *redis_key) {
         perror("Erro ao alocar memória para os parâmetros da thread");
         exit(EXIT_FAILURE);
     }
-    params->value = value;
-    params->redis_key = redis_key;
+    params->value = strdup(value);
+    params->redis_key = strdup(redis_key);
 
     if (pthread_create(&thread, NULL, store_in_redis_async, params) != 0) {
         perror("Erro ao criar a thread");
-        free(params); // Libera a memória alocada em caso de falha na criação da thread
+        free((char*)params->value);
+        free((char*)params->redis_key);
+        free(params);
         exit(EXIT_FAILURE);
     }
 
-    // opcional: Se não precisar esperar a thread terminar, você pode desanexá-la, por enquanto preferi deixar a thread rodando
     pthread_detach(thread);
 }
 
-static int
-connect_cb(void *rmsg, void * arg)
-{
-	printf("[Connected][%s]...\n", (char *)arg);
-	return 0;
+static int connect_cb(void *rmsg, void *arg) {
+    printf("[Connected][%s]...\n", (char *)arg);
+    return 0;
 }
 
-static int
-disconnect_cb(void *rmsg, void * arg)
-{
-	printf("[Disconnected][%s]...\n", (char *)arg);
-	return 0;
+static int disconnect_cb(void *rmsg, void *arg) {
+    printf("[Disconnected][%s]...\n", (char *)arg);
+    return 0;
 }
 
-static int
-msg_send_cb(void *rmsg, void * arg)
-{
-	clock_gettime(CLOCK_REALTIME, &end_time_pub);
-	printf("[Msg Sent][%s]...\n", (char *)arg);
-    //outra possibilidade seria capturar o tempo de publicação no callback
-    
-
-
-    //a msg foi enviada, alterando a flag de confirmação
+static int msg_send_cb(void *rmsg, void *arg) {
+    clock_gettime(CLOCK_REALTIME, &end_time_pub);
+    printf("[Msg Sent][%s]...\n", (char *)arg);
     *((int *)arg) = 1;
-	return 0;
+    return 0;
 }
 
+static nng_msg *mqtt_msg_compose(int type, int qos, char *topic, char *payload) {
+    nng_msg *msg;
+    nng_mqtt_msg_alloc(&msg, 0);
 
+    if (type == CONN) {
+        nng_mqtt_msg_set_packet_type(msg, NNG_MQTT_CONNECT);
+        nng_mqtt_msg_set_connect_proto_version(msg, 4);
+        nng_mqtt_msg_set_connect_keep_alive(msg, 30);
+        nng_mqtt_msg_set_connect_clean_session(msg, true);
+    } else if (type == PUB) {
+        nng_mqtt_msg_set_packet_type(msg, NNG_MQTT_PUBLISH);
+        nng_mqtt_msg_set_publish_dup(msg, 0);
+        nng_mqtt_msg_set_publish_qos(msg, qos);
+        nng_mqtt_msg_set_publish_retain(msg, 0);
+        nng_mqtt_msg_set_publish_topic(msg, topic);
+        nng_mqtt_msg_set_publish_payload(msg, (uint8_t *)payload, strlen(payload));
+    }
 
-static nng_msg *
-mqtt_msg_compose(int type, int qos, char *topic, char *payload)
-{
-	// Mqtt connect message
-	nng_msg *msg;
-	nng_mqtt_msg_alloc(&msg, 0);
-
-	if (type == CONN) {
-		nng_mqtt_msg_set_packet_type(msg, NNG_MQTT_CONNECT);
-
-		nng_mqtt_msg_set_connect_proto_version(msg, 4);
-		nng_mqtt_msg_set_connect_keep_alive(msg, 30);
-		nng_mqtt_msg_set_connect_clean_session(msg, true);
-	} else if (type == PUB) {
-		nng_mqtt_msg_set_packet_type(msg, NNG_MQTT_PUBLISH);
-
-		nng_mqtt_msg_set_publish_dup(msg, 0);
-		nng_mqtt_msg_set_publish_qos(msg, qos);
-		nng_mqtt_msg_set_publish_retain(msg, 0);
-		nng_mqtt_msg_set_publish_topic(msg, topic);
-		nng_mqtt_msg_set_publish_payload(
-		    msg, (uint8_t *) payload, strlen(payload));
-	}
-
-	return msg;
+    return msg;
 }
 
-void publish(const char *topic, int q, int *msg_sent){
-			//char *tempo_atual_varchar = tempo_para_varchar();
-            *msg_sent = 0;
-            char *byte_payload; //aqui pode usar uma função para colocar o valor que quiser em byte
-            byte_payload = "hehe";
-			nng_msg *msg = mqtt_msg_compose(PUB, q, (char *)topic, byte_payload);
-			//printf("O que esta sendo enviado: %s\n", byte_payload);
-			nng_sendmsg(*g_sock, msg, NNG_FLAG_ALLOC);
+void publish(const char *topic, int q, int *msg_sent) {
+    *msg_sent = 0;
+    char *byte_payload = "hehe"; // Usar uma função para gerar o payload se necessário
+    nng_msg *msg = mqtt_msg_compose(PUB, q, (char *)topic, byte_payload);
+    nng_sendmsg(*g_sock, msg, NNG_FLAG_ALLOC);
 
-            while (!(*msg_sent)) {
-                nng_msleep(1); // Sleep for a short duration to avoid busy waiting
-                }
-	        *msg_sent = 0; // Reset the flag
-            //free(byte_payload);
-};
+    while (!(*msg_sent)) {
+        nng_msleep(1); // Sleep for a short duration to avoid busy waiting
+    }
+    *msg_sent = 0; // Reset the flag
+}
 
-int
-client(int type, const char *url, const char *qos, const char *topic, const char *numero_pub, const char *interval, const char *redis_key)
-{
-
-	nng_socket  sock;
-	int         rv, sz, q, qpub, i;
-	nng_msg *   msg;
-	const char *arg = "CLIENT FOR QUIC";
-	uint32_t intervalo = atoi(interval);
+int client(int type, const char *url, const char *qos, const char *topic, const char *numero_pub, const char *interval, const char *redis_key) {
+    nng_socket sock;
+    int rv, sz, q, qpub, i;
+    nng_msg *msg;
+    const char *arg = "CLIENT FOR QUIC";
+    uint32_t intervalo = atoi(interval);
     int msg_sent = 0; // Flag to indicate message sent
 
-    //se a chave redis for nula
-    if (redis_key == NULL){
+    if (redis_key == NULL) {
         redis_key = "valores";
     }
-	/*
-	// Open a quic socket without configuration
-	if ((rv = nng_mqtt_quic_client_open(&sock, url)) != 0) {
-		printf("error in quic client open.\n");
-	}
-	
-	*/
-	qpub = atoi(numero_pub);
 
-	if(qpub<=0){
-		printf("Número de publicações inválido.\n");
-		printf("Número de publicações deve ser maior que 0.\n");
-		return 0;
-	
-	}
+    qpub = atoi(numero_pub);
+    if (qpub <= 0) {
+        printf("Número de publicações inválido.\n");
+        printf("Número de publicações deve ser maior que 0.\n");
+        return 0;
+    }
 
-	if ((rv = nng_mqtt_quic_client_open_conf(&sock, url, &config_user)) != 0) {
-		printf("error in quic client open.\n");
-	}
+    if ((rv = nng_mqtt_quic_client_open_conf(&sock, url, &config_user)) != 0) {
+        printf("error in quic client open.\n");
+    }
 
-	if (0 != nng_mqtt_quic_set_connect_cb(&sock, connect_cb, (void *)arg) ||
-	    0 != nng_mqtt_quic_set_disconnect_cb(&sock, disconnect_cb, (void *)arg) ||
-	    0 != nng_mqtt_quic_set_msg_send_cb(&sock, msg_send_cb, &msg_sent)) {
-		printf("error in quic client cb set.\n");
-	}
-	g_sock = &sock;
+    if (0 != nng_mqtt_quic_set_connect_cb(&sock, connect_cb, (void *)arg) ||
+        0 != nng_mqtt_quic_set_disconnect_cb(&sock, disconnect_cb, (void *)arg) ||
+        0 != nng_mqtt_quic_set_msg_send_cb(&sock, msg_send_cb, &msg_sent)) {
+        printf("error in quic client cb set.\n");
+    }
+    g_sock = &sock;
 
-	//Teste para descobrir o tempo de envio da mensagem de conexão
-
-	// MQTT Connect...
-	msg = mqtt_msg_compose(CONN, 0, NULL, NULL);
-	// Marcar o início do tempo
-    struct timespec start_time, end_time;
+    // MQTT Connect...
+    msg = mqtt_msg_compose(CONN, 0, NULL, NULL);
     clock_gettime(CLOCK_REALTIME, &start_time);
-	nng_sendmsg(sock, msg, NNG_FLAG_ALLOC);
-	clock_gettime(CLOCK_REALTIME, &end_time);
-	// Calcular a diferença
+    nng_sendmsg(sock, msg, NNG_FLAG_ALLOC);
+    clock_gettime(CLOCK_REALTIME, &end_time);
     long seconds = end_time.tv_sec - start_time.tv_sec;
     long nanoseconds = end_time.tv_nsec - start_time.tv_nsec;
-    double elapsed = seconds + nanoseconds*1e-9;
+    double elapsed = seconds + nanoseconds * 1e-9;
     time_connection = elapsed;
     printf("Time taken to connect: %.9f seconds\n", elapsed);
 
-	if (qos) {
-		q = atoi(qos);
-		if (q < 0 || q > 2) {
-			printf("Qos should be in range(0~2).\n");
-			q = 0;
-		}
-	}
+    if (qos) {
+        q = atoi(qos);
+        if (q < 0 || q > 2) {
+            printf("Qos should be in range(0~2).\n");
+            q = 0;
+        }
+    }
 
-	if (numero_pub){
+    for (i = 0; i < qpub; i++) {
+        clock_gettime(CLOCK_REALTIME, &start_time_pub);
+        publish(topic, q, &msg_sent);
+        clock_gettime(CLOCK_REALTIME, &end_time_pub);
+        long segundos_pub = end_time_pub.tv_sec - start_time_pub.tv_sec;
+        long nanosegundos_pub = end_time_pub.tv_nsec - start_time_pub.tv_nsec;
+        double diff_pub = segundos_pub + nanosegundos_pub * 1e-9;
 
-	
+        // Conversão do tipo(double em varchar)
+        char buf[64];
+        sprintf(buf, "%.9f", diff_pub);
+        printf("valor convertido: %s\n", buf);
 
-		
-		for(i=0;i<qpub;i++){
-		
-			
-		
-            
-            clock_gettime(CLOCK_REALTIME, &start_time_pub);
-			publish(topic,q,&msg_sent);
-            clock_gettime(CLOCK_REALTIME, &end_time_pub);
-            long segundos_pub = end_time_pub.tv_sec - start_time_pub.tv_sec;
-            long nanosegundos_pub = end_time_pub.tv_nsec - start_time_pub.tv_nsec;
-            double diff_pub = segundos_pub + nanosegundos_pub*1e-10;
+        // Salvar no Redis
+        store_in_redis_async_call(buf, redis_key);
 
-            //Conversão do tipo(double em varchar)
-            char *buf;
-            sprintf(buf,"%f", diff_pub);
-            printf("valor convertido:%s\n",buf);
+        printf("latencia do publisher: %.9f segundos\n", diff_pub);
+        nng_msleep(intervalo);
+    }
 
-            //Aqui eu posso salvar no redis
-            store_in_redis_async_call(buf, redis_key);
+    nng_close(sock);
+    printf("terminou o envio\n");
+    printf("Time taken to connect: %.9f seconds\n", time_connection);
+    fprintf(stderr, "Done.\n");
 
-            printf("latencia do publisher%.9f segundos\n",diff_pub);
-			nng_msleep(intervalo);
-
-			
-		
-		}
-	}
-	if ((rv = nng_mqtt_quic_client_open_conf(&sock, url, &config_user)) != 0) {
-		printf("error in quic client open.\n");
-	}
-
-#if defined(NNG_SUPP_SQLITE)
-	sqlite_config(&sock, MQTT_PROTOCOL_VERSION_v311);
-#endif
-
-     printf("terminou o envio\n");
-	 printf("Time taken to connect: %.9f seconds\n", time_connection);   	
-	//nng_msleep(0);
-	nng_close(sock);//aqui demora um pouco pois ele está desalocando os recursos
-	fprintf(stderr, "Done.\n");
-
-	return (0);
-
+    return 0;
 }
 
-static void
-printf_helper(char *exec)
-{
-	fprintf(stderr, "Usage: %s <url> <qos> <topic> <num_packets> <interval> <[redis-key]>\n", exec);
-	//fprintf(stderr, "Usage: %s <url> <qos> <topic>\n", exec);
-	exit(EXIT_FAILURE);
+static void printf_helper(char *exec) {
+    fprintf(stderr, "Usage: %s <url> <qos> <topic> <num_packets> <interval> <[redis-key]>\n", exec);
+    fprintf(stderr, "url: the broker url\n");
+    fprintf(stderr, "qos: qos level(0~2)\n");
+    fprintf(stderr, "topic: the topic name\n");
+    fprintf(stderr, "num_packets: the number of packet to publish\n");
+    fprintf(stderr, "interval: interval between sending two messages (ms)\n");
+    fprintf(stderr, "redis-key: Optional, Redis key to store the values\n");
 }
 
 int main(int argc, char **argv) {
-    if (argc < 5) {
-        goto error;
+    if (argc < 6) {
+        printf_helper(argv[0]);
+        return 0;
     }
-	
-    //client(PUB, argv[1], argv[2], argv[3], argv[4], argv[5]);
-	client(PUB, argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]);
-	//printf("Time taken to connect: %.9f seconds\n", time_connection);
-    
+    if (argc == 6) {
+        client(2, argv[1], argv[2], argv[3], argv[4], argv[5], NULL);
+    } else {
+        client(2, argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]);
+    }
+
     return 0;
-
-
-error:
-
-	printf_helper(argv[0]);
-	return 0;
 }
-
-
